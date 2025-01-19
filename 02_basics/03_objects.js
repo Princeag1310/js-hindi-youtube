51ff0434108f2e326d1a77d90d6f1a2a11761e6f
Prince Agrawal <princeagrawal1013@gmail.com>
2025-01-19 07:15:40 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,12 @@
+// singleton
+
+//object literals
+
+const JsUser = {
+    name:"Prince",
+    age : 20,
+    location : "london",
+    email : "Prince@Prince.com",
+    isLoggedIn : false,
+    lastLogInDays : ["Monday", "Saturday"]
+}
