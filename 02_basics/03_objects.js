701afd0e7f8bef577e74d8eb0c26f2e7c6e8fd9c
Prince Agrawal <princeagrawal1013@gmail.com>
2025-01-19 09:45:07 +0000

    objects Initial

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,11 +2,36 @@
 
 //object literals
 
+
+const mySym = Symbol("key1");
+
 const JsUser = {
     name:"Prince",
     age : 20,
+    [mySym] : "myKey1",
     location : "london",
     email : "Prince@Prince.com",
     isLoggedIn : false,
     lastLogInDays : ["Monday", "Saturday"]
 }
+
+console.log(JsUser["email"]);
+console.log(JsUser[mySym]);
+
+JsUser.email = "Prince@chatgpt.com";
+// Object.freeze(JsUser);
+JsUser.email = "Prince@microsoft.com";
+console.log(JsUser);
+
+
+JsUser.greeting = function(){
+    console.log("Hello JS User");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS User,${this.name} `);
+}
+
+JsUser.greeting();
+console.log(JsUser.greeting);
+console.log(JsUser.greetingTwo());
