2369565dd52d72e73ad7ca63300639903311348b
Prince Agrawal <princeagrawal1013@gmail.com>
2025-01-21 14:00:00 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -2,21 +2,65 @@
 // const b = 20;
 // var c = 30;
 // var c = 300;
-let a = 300;
+// let a = 300;
 
-if(true){
-    let a = 10;
-    const b = 20;
-    console.log("Inner : ",a);
-    // c= 30;
-}
+// if(true){
+//     let a = 10;
+//     const b = 20;
+//     console.log("Inner : ",a);
+//     // c= 30;
+// }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
-}
+// }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "Prince";
+
+    function two(){
+        const website = "youtube";
+        // console.log(username);
+    }
+    // console.log(website);
+
+    two();
+}
+one();
+
+if(true){
+    const username = "Prince";
+    if(username==="Prince"){
+        const website = " youtube";
+        // console.log(username+website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+
+// ++++++++++++++++++++++++++. Interesting   ++++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(addOne(5));
+function addOne(num){
+    return num+1;
+}
+
+
+// addTwo(5);
+
+const addTwo = function(num){
+    return num+2;
+}
+
+addTwo(5);
